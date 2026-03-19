f6ba3395845c7858a4d005693eee813e8af7adb7
Majo <designcardonaoro@gmail.com>
2026-03-19 08:36:25 -0500

    Implementación menu desplegable

--- my-app/src/components/JSX/Navbar.jsx
+++ my-app/src/components/JSX/Navbar.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Importa Link de react-router-dom
 import "./CSS/Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="navbar">
       <div className="navbar__logo">
         <img src="/Logo-Zehn-4x.svg" alt="Logo Zehn" className="navbar__logo-image" />
       </div>
-      <nav className="navbar__nav">
+      <div className="navbar__menu-icon" onClick={toggleMenu}>
+        <i className={menuOpen ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <nav className={`navbar__nav ${menuOpen ? "active" : ""}`}>
         <ul className="navbar__list">
           <li className="navbar__item"><Link to="/">Inicio</Link></li>
           <li className="navbar__item"><Link to="/events">Eventos</Link></li>
           <li className="navbar__item"><Link to="/gallery">Galeria</Link></li>
+          <li className="navbar__item navbar__cart">
+            <i className="fas fa-shopping-cart"></i>
+          </li>
         </ul>
       </nav>
-      <div className="navbar__cart">
-        <i className="fas fa-shopping-cart"></i>
-      </div>
     </header>
   );
 };
